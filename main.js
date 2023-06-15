a12599a15fa2dc3574538de1aba10fa89e91c5fa
babak bayat <baabaakbayat@gmail.com>
2023-06-15 19:44:10 +0330

    miamei

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ new ScrollMagic.Scene({
   .setClassToggle(".third", "show") // pins the element for the the scene's duration
   .addTo(controller2); // assign the scene to the controller
 
-var controller = new ScrollMagic.Controller({ addIndicators: true });
+var controller3 = new ScrollMagic.Controller({ addIndicators: true });
 
 // create a scene
 new ScrollMagic.Scene({
@@ -26,4 +26,13 @@ new ScrollMagic.Scene({
   //   offset: 200,
 })
   .setClassToggle(".fourth", "show") // pins the element for the the scene's duration
-  .addTo(controller); // assign the scene to the controller
+  .addTo(controller3); // assign the scene to the controller
+
+var controller4 = new ScrollMagic.Controller({ addIndicators: true });
+
+new ScrollMagic.Scene({
+  triggerElement: ".footer-hide",
+  triggerHook: 0.85,
+})
+  .setClassToggle(".footer-hide", "footer-show") // pins the element for the the scene's duration
+  .addTo(controller4); // assign the scene to the controller
