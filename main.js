ef9a1ebdcd43e3cdd4dc2f3e92e99b51525da6f0
babak bayat <baabaakbayat@gmail.com>
2023-06-15 12:38:35 +0330

    sdsdsd555

---
+++ main.js
@@ -0,0 +1,29 @@
+var controller = new ScrollMagic.Controller({ addIndicators: true });
+
+// create a scene
+new ScrollMagic.Scene({
+  triggerElement: ".seconde",
+  offset: 200,
+})
+  .setClassToggle(".seconde", "show") // pins the element for the the scene's duration
+  .addTo(controller); // assign the scene to the controller
+
+var controller2 = new ScrollMagic.Controller({ addIndicators: true });
+
+// create a scene
+new ScrollMagic.Scene({
+  triggerElement: ".third",
+  offset: 200,
+})
+  .setClassToggle(".third", "show") // pins the element for the the scene's duration
+  .addTo(controller2); // assign the scene to the controller
+
+var controller = new ScrollMagic.Controller({ addIndicators: true });
+
+// create a scene
+new ScrollMagic.Scene({
+  triggerElement: ".fourth",
+  offset: 200,
+})
+  .setClassToggle(".fourth", "show") // pins the element for the the scene's duration
+  .addTo(controller); // assign the scene to the controller
